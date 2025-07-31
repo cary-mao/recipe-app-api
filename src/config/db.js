cda1a93e33849dee5632395a9fff0a07913bd17b
cary-mao <cary_mao@163.com>
2025-07-31 16:47:11 +0800

    fix: neon name from serverless module

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import {drizzle} from 'drizzle-orm/neon-http'
-import {neno} from '@neondatabase/serverless'
+import {neon} from '@neondatabase/serverless'
 import {ENV} from './env.js'
 import * as schema from '../db/schema.js'
 
-const sql = neno(ENV.DATABASE_URL)
+const sql = neon(ENV.DATABASE_URL)
 export const db = drizzle(sql, {schema})
